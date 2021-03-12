09ef5d0cbb4fdf5ddffab4e412b7037cd04fcb88
denijal555 <61891424+denijal555@users.noreply.github.com>
2021-03-12 20:28:48 +0100

    Added first index.php file.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rent-A-Car Application</title>
+</head>
+<body>
+    <h1>Rent-a-Car</h1>
+    <hr>
+    <p>
+        This is my Web application.
+    </p>
+</body>
+</html>
